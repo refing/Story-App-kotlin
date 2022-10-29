c320f92b842b84d873dfdd8c9abecdaa389915a2
refing <r3f1nurghozi@gmail.com>
2022-10-30 02:33:07 +0700

    init v1

---
+++ app/src/main/java/com/example/storyapp/AddStoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.storyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
